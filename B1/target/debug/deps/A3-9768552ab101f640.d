3c2ef2ae3e24fc6de0fe7feb74e90ac04e5e4c52
Leonardo Kaftal <leonardokaftal1@gmail.com>
2023-12-16 16:23:18 +0100

    Added B1 solution (day 2 part 1)

---
+++ B1/target/debug/deps/A3-9768552ab101f640.d
@@ -0,0 +1,5 @@
+/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/libA3-9768552ab101f640.rmeta: src/main.rs
+
+/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/A3-9768552ab101f640.d: src/main.rs
+
+src/main.rs:
