150f1d773a5186d6f5a1249f46ec8125bcf89832
Leonardo Kaftal <95253599+LeonardoKaftal@users.noreply.github.com>
2023-12-16 17:06:17 +0100

    Delete A3 directory

--- A3/target/debug/deps/A3-603f98e3c77ad715.d
+++
@@ -1,5 +0,0 @@
-/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/A3-603f98e3c77ad715: src/main.rs
-
-/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/A3-603f98e3c77ad715.d: src/main.rs
-
-src/main.rs:
