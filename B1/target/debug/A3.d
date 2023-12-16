278e58efdf44bb555dbf16a0ff7c3abcc9e84ad6
Leonardo Kaftal <95253599+LeonardoKaftal@users.noreply.github.com>
2023-12-16 16:23:58 +0100

    Delete B1/target directory

--- B1/target/debug/A3.d
+++
@@ -1,1 +0,0 @@
-/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/A3: /home/leo/Desktop/Rust/AOC-2023/A3/src/main.rs
--- B1/target/debug/deps/A3-3b565354a4a7c34f.d
+++
@@ -1,5 +0,0 @@
-/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/libA3-3b565354a4a7c34f.rmeta: src/main.rs
-
-/home/leo/Desktop/Rust/AOC-2023/A3/target/debug/deps/A3-3b565354a4a7c34f.d: src/main.rs
-
-src/main.rs:
